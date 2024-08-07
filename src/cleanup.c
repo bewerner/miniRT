b32db874a9432e82e169ff4b5f0444aa2ea3fcb0
nmihaile <nmihaile@2-E-3.42heilbronn.de>
2024-08-07 15:52:55 +0200

    adds inital parsing structure

--- src/cleanup.c
+++ src/cleanup.c
@@ -3,23 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   cleanup.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bwerner <bwerner@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 16:14:02 by bwerner           #+#    #+#             */
-/*   Updated: 2024/08/05 15:00:12 by bwerner          ###   ########.fr       */
+/*   Updated: 2024/08/07 15:37:42 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/miniRT.h"
 
+char	*error_msg(t_error error)
+{
+	const char	*msgs[3] = {
+		RT_ERROR_MSG_SUCCES,
+		RT_ERROR_MSG_WRONG_FILE_FORMAT,
+		RT_ERROR_MSG_AMBIENT_LOWER_CASE
+	};
+
+	return ((char*)msgs[error]);
+}
+
 void	error(char *message)
 {
 	if (!message)
 		return ;
-	ft_putstr_fd("miniRT: ", STDERR_FILENO);
+	ft_putendl_fd("Error", STDERR_FILENO);
 	if (errno)
 		perror(message);
-	else
+	else if (message)
 		ft_putendl_fd(message, STDERR_FILENO);
 	errno = 0;
 }
@@ -30,10 +41,10 @@ void	cleanup(t_rt *rt)
 	free(rt->lights);
 }
 
-void	terminate(char* message, uint8_t exit_code, t_rt *rt)
+void	terminate(char *msg, uint8_t exit_code, t_rt *rt)
 {
-	if (message)
-		error(message);
+	if (msg )
+		error(msg);
 	if (rt->mlx)
 		mlx_terminate(rt->mlx);
 	cleanup(rt);
