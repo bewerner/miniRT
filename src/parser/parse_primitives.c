0e5271fbab1c295cde1b3a774be903d8689ffa2d
nmihaile <148884235+nmihaile@users.noreply.github.com>
2024-08-09 17:00:31 +0200

    allows to load cylinders in .rt files

--- src/parser/parse_primitives.c
+++ src/parser/parse_primitives.c
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 12:29:18 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/08/09 12:42:03 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/08/09 16:59:45 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,37 @@ t_error	parse_plane(size_t *obj_count, t_rt *rt)
 	rt->objects[*obj_count].normal.z = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
 	rt->objects[*obj_count].normal = vec3_normalize(rt->objects[*obj_count].normal);
 	rt->objects[*obj_count].dist = vec3_dot(rt->objects[*obj_count].origin, rt->objects[*obj_count].normal);
+	rt->objects[*obj_count].base_color.r = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
+	rt->objects[*obj_count].base_color.g = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
+	rt->objects[*obj_count].base_color.b = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
+	rt->objects[*obj_count].base_color.a = 1.0f;
+	(*obj_count)++;
+	return (RT_SUCCESS);
+}
+
+t_error	parse_cylinder(size_t *obj_count, t_rt *rt)
+{
+	char			*line;
+	static size_t	count;
+
+	count++;
+	line = &rt->line->content[2];
+	rt->objects[*obj_count].type = OBJ_CYLINDER;
+	rt->objects[*obj_count].origin.x = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
+	rt->objects[*obj_count].origin.y = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
+	rt->objects[*obj_count].origin.z = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
+	
+	rt->objects[*obj_count].orientation.x = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
+	rt->objects[*obj_count].orientation.y = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
+	rt->objects[*obj_count].orientation.z = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
+	rt->objects[*obj_count].orientation = vec3_normalize(rt->objects[*obj_count].orientation);
+
+	rt->objects[*obj_count].radius = validate_range(get_next_value(&line, rt) * 0.5f, (t_vec2){-INFINITY, INFINITY}, rt);
+	rt->objects[*obj_count].height = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
+	
+	rt->objects[*obj_count].cap1 = vec3_sub(rt->objects[*obj_count].origin, vec3_scale(rt->objects[*obj_count].height / 2, rt->objects[*obj_count].orientation));
+	rt->objects[*obj_count].cap2 = vec3_add(rt->objects[*obj_count].origin, vec3_scale(rt->objects[*obj_count].height / 2, rt->objects[*obj_count].orientation));
+	
 	rt->objects[*obj_count].base_color.r = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
 	rt->objects[*obj_count].base_color.g = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
 	rt->objects[*obj_count].base_color.b = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
