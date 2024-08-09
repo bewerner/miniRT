9c65be0204d5ef8c23cebc21416b7692add8a84f
Benjamin Werner <wernerbenjamin42@gmail.com>
2024-08-09 23:02:39 +0200

    vec4_to_rgba now has dither switch adds vec4_to_abgr (reverse format for pixel buffer) background is now ambient color instead of black

--- src/utils/color_convert.c
+++ src/utils/color_convert.c
@@ -6,7 +6,7 @@
 /*   By: bwerner <bwerner@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 22:04:33 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/08/09 15:51:12 by bwerner          ###   ########.fr       */
+/*   Updated: 2024/08/09 22:53:59 by bwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,47 +26,6 @@ static double	clamp(double value, double min, double max)
 	return (value);
 }
 
-
-
-
-
-// static uint8_t	rand_dither(double value)
-// {
-// 	static size_t	i;
-// 	static double	arr[1000];
-// 	static bool		initialized;
-
-// 	if (!initialized)
-// 	{
-// 		while (i < 1000)
-// 		{
-// 			arr[i] = (double)rand() / RAND_MAX;
-// 			i++;
-// 		}
-// 		initialized = true;
-// 		i = 0;
-// 	}
-// 	i++;
-// 	if (arr[i % 1000] <= value - (uint8_t)value)
-// 		value++;
-// 	return ((uint8_t)value);
-// }
-
-
-
-
-
-// static uint8_t	rand_dither(double value)
-// {
-// 	if ((double)rand() / RAND_MAX <= value - (uint8_t)value)
-// 		value++;
-// 	return ((uint8_t)value);
-// }
-
-
-
-
-
 static uint8_t	rand_dither(double value)
 {
 	static int	rand;
@@ -76,17 +35,42 @@ static uint8_t	rand_dither(double value)
 	return ((uint8_t)value);
 }
 
-uint32_t	vec4_to_rgba(t_vec4	col)
+uint32_t	vec4_to_abgr(t_vec4	col, bool dither)
 {
-	if (true)
+	if (dither)
+	{
+		return (0
+			| ((uint8_t)(rand_dither(clamp(col.r, 0, 1) * 255)))
+			| ((uint8_t)(rand_dither(clamp(col.g, 0, 1) * 255)) << 8)
+			| ((uint8_t)(rand_dither(clamp(col.b, 0, 1) * 255)) << 16)
+			| ((uint8_t)(clamp(col.a, 0, 1) * 255)) << 24);
+	}
+	else
+	{
+		return (0
+			| ((uint8_t)(clamp(col.r, 0, 1) * 255))
+			| ((uint8_t)(clamp(col.g, 0, 1) * 255) << 8)
+			| ((uint8_t)(clamp(col.b, 0, 1) * 255) << 16)
+			| ((uint8_t)(clamp(col.a, 0, 1) * 255) << 24));
+	}
+}
+
+uint32_t	vec4_to_rgba(t_vec4	col, bool dither)
+{
+	if (dither)
+	{
 		return (0
 			| ((uint8_t)(rand_dither(clamp(col.r, 0, 1) * 255)) << 24)
 			| ((uint8_t)(rand_dither(clamp(col.g, 0, 1) * 255)) << 16)
 			| ((uint8_t)(rand_dither(clamp(col.b, 0, 1) * 255)) << 8)
 			| (uint8_t)(clamp(col.a, 0, 1) * 255));
-	return (0
-		| ((uint8_t)(clamp(col.r, 0, 1) * 255) << 24)
-		| ((uint8_t)(clamp(col.g, 0, 1) * 255) << 16)
-		| ((uint8_t)(clamp(col.b, 0, 1) * 255) << 8)
-		| (uint8_t)(clamp(col.a, 0, 1) * 255));
+	}
+	else
+	{
+		return (0
+			| ((uint8_t)(clamp(col.r, 0, 1) * 255) << 24)
+			| ((uint8_t)(clamp(col.g, 0, 1) * 255) << 16)
+			| ((uint8_t)(clamp(col.b, 0, 1) * 255) << 8)
+			| (uint8_t)(clamp(col.a, 0, 1) * 255));
+	}
 }
