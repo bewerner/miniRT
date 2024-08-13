829e3ed49b980f3b81e2d8594a05584a0570f5f1
Benjamin Werner <wernerbenjamin42@gmail.com>
2024-08-13 18:23:07 +0200

    cylinder caps are now initiated in parse_cylinder

--- src/parser/parse_primitives.c
+++ src/parser/parse_primitives.c
@@ -6,7 +6,7 @@
 /*   By: bwerner <bwerner@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 12:29:18 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/08/13 17:16:16 by bwerner          ###   ########.fr       */
+/*   Updated: 2024/08/13 18:20:10 by bwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,13 +83,20 @@ t_error	parse_cylinder(t_cylinder *cylinder, t_rt *rt)
 	cylinder->radius = validate_range(get_next_value(&line, rt) * 0.5f, (t_vec2){-INFINITY, INFINITY}, rt);
 	cylinder->height = validate_range(get_next_value(&line, rt), (t_vec2){-INFINITY, INFINITY}, rt);
 	
-	cylinder->cap1 = vec3_sub(cylinder->origin, vec3_scale(cylinder->height / 2, cylinder->orientation));
-	cylinder->cap2 = vec3_add(cylinder->origin, vec3_scale(cylinder->height / 2, cylinder->orientation));
-	
 	cylinder->base_color.r = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
 	cylinder->base_color.g = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
 	cylinder->base_color.b = validate_range(get_next_value(&line, rt) / 255.0f, (t_vec2){0.0f, 1.0f}, rt);
 	cylinder->base_color.a = 1.0f;
+
+	cylinder->cap1.origin = vec3_sub(cylinder->origin, vec3_scale(cylinder->height / 2, cylinder->orientation));
+	cylinder->cap2.origin = vec3_add(cylinder->origin, vec3_scale(cylinder->height / 2, cylinder->orientation));
+	cylinder->cap1.normal = vec3_scale(-1, cylinder->orientation);
+	cylinder->cap2.normal = cylinder->orientation;
+	cylinder->cap1.dist = vec3_dot(cylinder->cap1.origin, cylinder->cap1.normal);
+	cylinder->cap2.dist = vec3_dot(cylinder->cap2.origin, cylinder->cap2.normal);
+	cylinder->cap1.base_color = cylinder->base_color;
+	cylinder->cap2.base_color = cylinder->base_color;
+
 	return (RT_SUCCESS);
 }
 
