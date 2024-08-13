7ffc40d74df7da0b7b55489f6b39a98859677a69
Benjamin Werner <wernerbenjamin42@gmail.com>
2024-08-13 17:56:28 +0200

    all objects are no visible from the inside and correctly block off light from the outside to the inside and vice versa get_diffuse_color: added intensity threshold check for improved performance in very dark scenes

--- src/primitives/get_hitpoint.c
+++ src/primitives/get_hitpoint.c
@@ -6,15 +6,14 @@
 /*   By: bwerner <bwerner@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 18:27:07 by bwerner           #+#    #+#             */
-/*   Updated: 2024/08/13 17:16:40 by bwerner          ###   ########.fr       */
+/*   Updated: 2024/08/13 17:32:02 by bwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/miniRT.h"
 
-bool	is_obstructed(t_ray ray, t_object *exclude, t_rt *rt)
+bool	is_obstructed(t_ray ray, t_rt *rt)
 {
-	// size_t		i;
 	t_hitpoint	current;
 	float		ray_len;
 	t_object	*object;
@@ -23,15 +22,9 @@ bool	is_obstructed(t_ray ray, t_object *exclude, t_rt *rt)
 	current.pos = VEC3_INF;
 	current.object = NULL;
 	ray_len = vec3_len(ray.dir);
-	// i = 0;
 	object = rt->objects;
 	while (object)
 	{
-		if (object == exclude)
-		{
-			object = object->next;
-			continue ;
-		}
 		if (object->type == OBJ_SPHERE )
 			current = get_hitpoint_sphere(ray, (t_sphere *)object);
 		else if (object->type == OBJ_PLANE)
@@ -42,23 +35,6 @@ bool	is_obstructed(t_ray ray, t_object *exclude, t_rt *rt)
 			return (true);
 		object = object->next;
 	}
-	// while (rt->objects[i].type)
-	// {
-	// 	if (&rt->objects[i] == exclude)
-	// 	{
-	// 		i++;
-	// 		continue ;
-	// 	}
-	// 	if (rt->objects[i].type == OBJ_SPHERE )
-	// 		current = get_hitpoint_sphere(ray, &rt->objects[i]);
-	// 	else if (rt->objects[i].type == OBJ_PLANE)
-	// 		current = get_hitpoint_plane(ray, &rt->objects[i]);
-	// 	else if (rt->objects[i].type == OBJ_CYLINDER)
-	// 		current = get_hitpoint_cylinder(ray, &rt->objects[i]);
-	// 	if (vec3_len(current.ray) < ray_len)
-	// 		return (true);
-	// 	i++;
-	// }
 	return (false);
 }
 
