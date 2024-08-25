ef35cb4cc696b82d5bebad1a0fe700b2f97d497f
nmihaile <nmihaile@2-E-3.42heilbronn.de>
2024-08-25 20:40:17 +0200

    adds shader utils srcs and draws uv screen

---
+++ src/shader/shader_program.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shader_program.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 14:39:42 by nmihaile          #+#    #+#             */
+/*   Updated: 2024/08/25 20:14:03 by nmihaile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../inc/miniRT.h"
+
+void	create_shader_program(t_rt *rt)
+{
+	const char	*vert_files[] = {
+		"shaders/vertex/screen.vert",
+		NULL};
+	const char	*frag_files[] = {
+		"shaders/fragment/fragment.frag",
+		NULL};
+	GLuint		shader_program;
+	GLuint		shaders[2];
+	char		*vert_src;
+	char		*frag_src;
+
+	shaders[0] = 0;
+	shaders[1] = 0;
+
+	// LOAD & ASSEMBLE SHADER SOURCES
+
+	vert_src = assemble_shader_source(vert_files);
+	frag_src = assemble_shader_source(frag_files);
+
+	// printf("%s\n", vert_src);
+
+	if (vert_src == NULL || frag_src == NULL)
+	{
+		if (vert_src)
+			free(vert_src);
+		if (frag_src)
+			free(frag_src);
+		terminate("failed to assemble shader sources" ,1, rt);
+	}
+
+	// CREATE SHADERS FROM SOURCE_STRING
+
+	shaders[0] = compile_shader_src(GL_VERTEX_SHADER, vert_src);
+	shaders[1] = compile_shader_src(GL_FRAGMENT_SHADER, frag_src);
+	free(vert_src);
+	free(frag_src);
+	if (shaders[0] == 0 || shaders[1] == 0)
+	{
+		if (shaders[0] > 0)
+			glDeleteShader(shaders[0]);
+		if (shaders[1] > 0)
+			glDeleteShader(shaders[1]);
+		terminate("failed to compile shaders", 1, rt);
+	}
+
+	// CREATE SHADER PROGRAM AND ATTACH SHADERS
+
+	shader_program = glCreateProgram();
+	glAttachShader(shader_program, shaders[0]);
+	glAttachShader(shader_program, shaders[1]);
+
+	// COMPILE SHADER PROGRAM
+
+	int		success;
+	// char	errorlog[512];
+	glLinkProgram(shader_program);
+	glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		GLint log_length;
+		glGetProgramiv(shader_program, GL_INFO_LOG_LENGTH, &log_length);
+		char *errorlog = malloc(log_length);
+		glGetProgramInfoLog(shader_program, log_length, NULL, errorlog);
+		printf("ERROR SHADER PROGRAMM LINK FAILED\n%s\n", errorlog);
+		free(errorlog);
+
+		glDeleteShader(shaders[0]);
+		glDeleteShader(shaders[1]);
+		terminate("failed to create shader program", 1, rt);
+	}
+
+	// USE SHADER_PROGRAM && DELETE SHADER OBJECTS
+	glUseProgram(shader_program);
+	glDeleteShader(shaders[0]);
+	glDeleteShader(shaders[1]);
+	rt->shader_program = shader_program;
+}
