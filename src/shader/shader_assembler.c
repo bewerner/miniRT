cdd6c2de25546c2eed76ab998f076afd91380aeb
nmihaile <nmihaile@2-E-3.42heilbronn.de>
2024-08-27 14:51:45 +0200

    changes shader_assembler to accept #imports

--- src/shader/shader_assembler.c
+++ src/shader/shader_assembler.c
@@ -6,36 +6,11 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 12:52:05 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/08/25 19:37:55 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/08/27 14:50:25 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/miniRT.h"
-
-static size_t	combined_src_size(const char **files)
-{
-	int		fd;
-	size_t	total_size;
-	off_t	curr_size;
-
-	total_size = 0;
-	while (*files)
-	{
-		fd = open(*files, O_RDONLY);
-		if (fd == -1)
-			return (0);
-		curr_size = lseek(fd, 0, SEEK_END);
-		if (curr_size == -1)
-		{
-			close(fd);
-			return (0);
-		}
-		total_size += curr_size;
-		close(fd);
-		files++;
-	}
-	return (total_size);
-}
 
 static size_t	read_shader_src(char *buf, const char *file)
 {
@@ -52,31 +27,118 @@ static size_t	read_shader_src(char *buf, const char *file)
 	return (size);
 }
 
-char	*assemble_shader_source(const char **files)
+size_t	file_size(const char *file)
 {
+	int		fd;
+	off_t	size;
+
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	size = lseek(fd, 0, SEEK_END);
+	if (size == -1)
+		size = 0;
+	close(fd);
+	return ((size_t)size);
+}
+
+int	is_import(char *str)
+{
+	return (ft_strncmp(str, "#import ", 8) == 0);
+}
+
+size_t	shader_src_size_with_import(const char *file)
+{
+	int		fd;
+	size_t	size;
+	size_t	line_size;
+	char	*line;
+
+	size = 0;
+	fd = open(file, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	line = get_next_line(fd);
+	while (line)
+	{
+		line_size = ft_strlen(line);
+		if (is_import(line))
+		{
+			if (ft_strlen(line) > 8)
+			{
+				if (line[ft_strlen(line) - 1] == '\n')
+					line[ft_strlen(line) - 1] = '\0';
+				line_size = file_size(&line[8]);
+				printf("%s -> %lu\n", &line[8], line_size);
+			}
+			else
+				line_size = 0;
+		}
+		size += line_size;
+		free(line);
+		if (line_size == 0)
+			return (0);
+		line = get_next_line(fd);
+	}
+	close(fd);
+	return (size);
+}
+
+int	read_into_buf(char *buf, int fd)
+{
+	size_t	i;
+	size_t	j;
+	char	*line;
+
+	i = 0;
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (is_import(line))
+		{
+			if (line[ft_strlen(line) - 1] == '\n')
+				line[ft_strlen(line) - 1] = '\0';
+			j = read_shader_src(&buf[i], &line[8]);
+			if (j == 0)
+			{
+				return (close(fd), -1);
+			}
+			i += j;
+		}
+		else
+		{
+			ft_memcpy(&buf[i], line, ft_strlen(line));
+			i += ft_strlen(line);
+		}
+		free(line);
+		line = get_next_line(fd);
+	}
+	buf[i] = '\0';
+	return (0);
+}
+
+char	*assemble_shader_source(const char *file)
+{
+	int		fd;
 	char	*shader_src;
 	size_t	src_size;
-	size_t	read_size;
-	size_t	curr_pos;
 
-	src_size = combined_src_size(files);
+	src_size = shader_src_size_with_import(file);
 	if (src_size == 0)
 		return (NULL);
 	shader_src = (char *)malloc(src_size + 1 * sizeof(char));
-	if (shader_src == NULL)
-		return (NULL);
-	curr_pos = 0;
-	while (*files)
+	fd = open(file, O_RDONLY);
+	if (shader_src == NULL || fd == -1)
 	{
-		read_size = read_shader_src(&shader_src[curr_pos], *files);
-		if (read_size == 0)
-		{
+		if (shader_src)
 			free(shader_src);
-			return (NULL);
-		}
-		curr_pos += read_size;
-		files++;
+		return (NULL);
 	}
-	shader_src[curr_pos] = '\0';
+	if (read_into_buf(shader_src, fd) == -1)
+	{
+		free (shader_src);
+		shader_src = NULL;
+	}
+	close(fd);
 	return (shader_src);
 }
