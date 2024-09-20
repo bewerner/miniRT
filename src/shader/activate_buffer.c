caf187c2a741958747cb96c29b783b58672eeee1
Benjamin Werner <wernerbenjamin42@gmail.com>
2024-09-20 18:09:47 +0200

    fixes issues on 42macs by not dumping (now nonexistant) buffers in each shader. also commented out seemingly unnecessary (?) lines in activate_buffer.c

--- src/shader/activate_buffer.c
+++ src/shader/activate_buffer.c
@@ -6,7 +6,7 @@
 /*   By: bwerner <bwerner@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 01:39:19 by bwerner           #+#    #+#             */
-/*   Updated: 2024/09/20 02:01:59 by bwerner          ###   ########.fr       */
+/*   Updated: 2024/09/20 18:03:48 by bwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,42 +23,43 @@ void	activate_objects(GLuint shader_program, t_rt *rt)
 {
 	GLint uniform_location;
 
-	glBindBuffer(GL_TEXTURE_BUFFER, rt->tbo_objects_id);
-	glActiveTexture(GL_TEXTURE1);
-	glTexBuffer(GL_TEXTURE_BUFFER, GL_R32F, rt->tbo_objects_id);
+	// glBindBuffer(GL_TEXTURE_BUFFER, rt->tbo_objects_id);
+	// glActiveTexture(GL_TEXTURE1);
+	// glTexBuffer(GL_TEXTURE_BUFFER, GL_R32F, rt->tbo_objects_id);
+	(void)rt;
 	uniform_location = glGetUniformLocation(shader_program, "objects");
 	if (uniform_location == -1)
 		terminate("objects not found in shader program", 1, rt);
 	glUniform1i(uniform_location, 1);
-	glBindBuffer(GL_TEXTURE_BUFFER, 0);	// unbind
+	// glBindBuffer(GL_TEXTURE_BUFFER, 0);	// unbind
 }
 
 void	activate_lights(GLuint shader_program, t_rt *rt)
 {
 	GLint uniform_location;
 
-	glBindBuffer(GL_TEXTURE_BUFFER, rt->tbo_lights_id);
-	glActiveTexture(GL_TEXTURE2);
-	glTexBuffer(GL_TEXTURE_BUFFER, GL_R32F, rt->tbo_lights_id);
+	// glBindBuffer(GL_TEXTURE_BUFFER, rt->tbo_lights_id);
+	// glActiveTexture(GL_TEXTURE2);
+	// glTexBuffer(GL_TEXTURE_BUFFER, GL_R32F, rt->tbo_lights_id);
+	(void)rt;
 	uniform_location = glGetUniformLocation(shader_program, "lights");
 	if (uniform_location == -1)
 		terminate("lights not found in shader program", 1, rt);
 	glUniform1i(uniform_location, 2);
-	glBindBuffer(GL_TEXTURE_BUFFER, 0);	// unbind
+	// glBindBuffer(GL_TEXTURE_BUFFER, 0);	// unbind
 }
 
 void	activate_agx_lut(GLuint shader_program, t_rt *rt)
 {
 	GLint uniform_location;
 
-	if (shader_program != rt->postprocessing_shader_program)
-		return ;
-	glBindBuffer(GL_TEXTURE_BUFFER, rt->tbo_agx_lut_id);
-	glActiveTexture(GL_TEXTURE3);
-	glTexBuffer(GL_TEXTURE_BUFFER, GL_RGB32F, rt->tbo_agx_lut_id);
+	// glBindBuffer(GL_TEXTURE_BUFFER, rt->tbo_agx_lut_id);
+	// glActiveTexture(GL_TEXTURE3);
+	// glTexBuffer(GL_TEXTURE_BUFFER, GL_RGB32F, rt->tbo_agx_lut_id);
+	(void)rt;
 	uniform_location = glGetUniformLocation(shader_program, "agx_lut");
 	if (uniform_location == -1)
 		terminate("agx_lut not found in shader program", 1, rt);
 	glUniform1i(uniform_location, 3);
-	glBindBuffer(GL_TEXTURE_BUFFER, 0);	// unbind
+	// glBindBuffer(GL_TEXTURE_BUFFER, 0);	// unbind
 }
