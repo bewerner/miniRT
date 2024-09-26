d700a16a0043292d67a0fc91bd004d3e601acf79
nmihaile <148884235+nmihaile@users.noreply.github.com>
2024-09-26 19:25:35 +0200

    adds correct bumb_map_id value (-1) to default material

--- src/parser/parse_material2.c
+++ src/parser/parse_material2.c
@@ -6,7 +6,7 @@
 /*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 11:54:03 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/09/25 11:54:21 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/09/26 19:24:06 by nmihaile         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,4 +32,5 @@ void	create_default_material(size_t mat_cnt, t_material *mat)
 	mat->emission_color.g = 0.0f;
 	mat->emission_color.b = 0.0f;
 	mat->color_texture_id = -1;
+	mat->bumb_map_id = -1;
 }
