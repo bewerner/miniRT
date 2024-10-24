e83e2be4bd51f013575bd141ce67ecd8d4d859ad
Benjamin Werner <wernerbenjamin42@gmail.com>
2024-10-24 07:47:32 +0200

    updates resize_framebuffer (called in resize_hook). resizing window works again

--- src/hooks/resize_hook.c
+++ src/hooks/resize_hook.c
@@ -3,31 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   resize_hook.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nmihaile <nmihaile@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: bwerner <bwerner@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 19:46:24 by nmihaile          #+#    #+#             */
-/*   Updated: 2024/09/28 14:07:19 by nmihaile         ###   ########.fr       */
+/*   Updated: 2024/10/24 07:44:30 by bwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/miniRT.h"
 
-void	resize_framebuffer_texture(t_rt *rt)
+static void	resize_framebuffer(t_rt *rt)
 {
+	glDeleteFramebuffers(1, &rt->fbo_id);
 	glDeleteTextures(1, &rt->tex_fbo_id);
-	glGenTextures(1, &rt->tex_fbo_id);
-	glBindTexture(GL_TEXTURE_2D, rt->tex_fbo_id);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, rt->width, rt->height, 0,
-		GL_RGB, GL_UNSIGNED_BYTE, NULL);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-	glBindTexture(GL_TEXTURE_2D, 0);
-	glBindFramebuffer(GL_FRAMEBUFFER, rt->fbo_id);
-	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
-		GL_TEXTURE_2D, rt->tex_fbo_id, 0);
-	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
-		printf("Framebuffer not complete after resize!\n");
-	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	create_fbo(rt);
 }
 
 void	resize_hook(GLFWwindow *window, int width, int height)
@@ -41,6 +30,6 @@ void	resize_hook(GLFWwindow *window, int width, int height)
 	glViewport(0, 0, width, height);
 	glfwGetFramebufferSize(window, &rt->width, &rt->height);
 	rt->aspect_ratio = (float)rt->width / (float)rt->height;
-	resize_framebuffer_texture(rt);
+	resize_framebuffer(rt);
 	rt->sample_count = 0;
 }
