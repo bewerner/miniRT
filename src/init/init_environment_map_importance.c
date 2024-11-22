a0fb88abc44597b873956828b1591feb8c03ef16
Benjamin Werner <wernerbenjamin42@gmail.com>
2024-11-22 01:11:19 +0100

    clean up init_environment_map

--- src/init/init_environment_map_importance.c
+++ src/init/init_environment_map_importance.c
@@ -6,7 +6,7 @@
 /*   By: bwerner <bwerner@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 16:21:23 by bwerner           #+#    #+#             */
-/*   Updated: 2024/11/14 20:32:43 by bwerner          ###   ########.fr       */
+/*   Updated: 2024/11/22 01:10:35 by bwerner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,105 +16,57 @@
 // weights.g = weight (resolution compensated PDF)
 // weights.b = ?
 
-void	compensate_weights(t_vec3 *weights, int width, int height)
+void	compensate_weights(t_vec3 *weights, size_t length)
 {
-	size_t	length;
-	size_t	i;
-	double	scale;
-	double  biggest = 0;
-	double  average = 0;
+	double scale = (double)length / (2 * M_PI);
 
-	scale = (double)width * height / (2 * M_PI);
-	length = (size_t)width * height;
-	i = 0;
-	while (i < length)
-	{
+	for (size_t i = 0; i < length; i++)
 		weights[i].g *= scale;
-		if (weights[i].g > biggest)
-			biggest = weights[i].g;
-		average += weights[i].g;
-		i++;
-	}
-	average /= length;
-	printf("scale: %f\n", scale);
-	printf("biggest weight: %f\n", biggest);
-	printf("average weight: %f\n", average);
 }
 
-void	set_cumulative_distribution(t_vec3 *weights, int width, int height)
+void	set_cumulative_distribution(t_vec3 *weights, size_t length)
 {
-	size_t	length;
-	size_t	i;
-	double	previous_weight;
-	double	current_weight;
+	double	previous_weight = 0;
+	double	current_weight = 0;
 
-	length = (size_t)width * height;
-	i = 0;
-	previous_weight = 0;
-	current_weight = 0;
-	while (i < length)
+	for (size_t i = 0; i < length; i++)
 	{
 		weights[i].g = weights[i].r;
 		current_weight = previous_weight + weights[i].r;
 		weights[i].r = current_weight;
 		previous_weight = current_weight;
-		i++;
 	}
 }
 
-static void	normalize_weights(t_vec3 *weights, double total_weight, int width, int height)
+static void	normalize_weights(t_vec3 *weights, double total_weight, size_t length)
 {
-	size_t	length;
-	size_t	i;
-
-	length = (size_t)width * height;
-	i = 0;
-	while (i < length)
-	{
+	for (size_t i = 0; i < length; i++)
 		weights[i].r /= total_weight;
-		i++;
-	}
 }
 
-static double	get_importance_weight(double luminance, double row, int width, int height)
+static double get_importance_weight(t_vec3 color, double row, int width, int height)
 {
-	double	solid_angle;
-	double	importance_weight;
-	static double	biggest;
+	double luminance = color.r * 0.2126 + color.g * 0.7152 + color.b * 0.0722;
+	double solid_angle = sin((row / height) * M_PI) * (M_PI / height) * (2.0 * M_PI / width);
 
-	if (luminance > biggest)
-		biggest = luminance;
-	if (luminance == -1)
-		printf("biggest luminance: %f\n", biggest);
-	solid_angle = sinf(row / height * M_PI) * (M_PI / height) * (2 * M_PI / width);
-	importance_weight = luminance * solid_angle;
-	return (importance_weight);
+	return (luminance * solid_angle);
 }
 
-void	set_importance_weight(t_vec3 *image, t_vec3 *weights, int width, int height)
+void	set_importance_weight(t_vec3 *image, t_vec3 *weights, int width, int height, size_t length)
 {
-	t_vec3	*pixel;
+	t_vec3	color;
 	t_vec3	*weight;
-	int		x;
-	int		y;
-	double	total_weight;
+	double	total_weight = 0;
 
-	total_weight = 0;
-	y = 0;
-	while (y < height)
+	for (int y = 0; y < height; y++)
 	{
-		x = 0;
-		while (x < width)
+		for (int x = 0; x < width; x++)
 		{
-			pixel = &image[x + y * width];
+			color = image[x + y * width];
 			weight = &weights[x + y * width];
-			weight->r = pixel->r * 0.2126 + pixel->g * 0.7152 + pixel->b * 0.0722;
-			weight->r = get_importance_weight(weight->r, y + 1, width, height);
+			weight->r = get_importance_weight(color, y + 0.5f, width, height);
 			total_weight += weight->r;
-			x++;
 		}
-		y++;
 	}
-	get_importance_weight(-1, y + 1, width, height);
-	normalize_weights(weights, total_weight, width, height);
+	normalize_weights(weights, total_weight, length);
 }
