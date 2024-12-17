59c00ac18fd42c632116e2fef62269329e9218b3
Benjamin Werner <wernerbenjamin42@gmail.com>
2024-12-17 09:02:39 +0100

    update readme

--- script.sh
+++ script.sh
@@ -1,9 +1,5 @@
 ./miniRT scenes/primitives.rt
 ./miniRT scenes/checkerboard.rt
-./miniRT scenes/soft_light.rt
 ./miniRT scenes/poker.rt
 ./miniRT scenes/poker2.rt
 ./miniRT scenes/road3.rt
-
-# infinity mirror
-# caleidoscope
